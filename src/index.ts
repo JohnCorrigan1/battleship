e996dbcebd2e2c76facd9c1573ac214ef8c0d34c
John Corrigan <jcorrigan322@gmail.com>
2022-09-17 16:03:48 -0400

    progress on ui

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,5 @@
-import './styles.css'
-import { GameBoard } from './factories/gameboard'
-import { Ship } from './factories/ship'
-import { Player } from './factories/players'
+// import './styles.css'
+import renderGrids from "./ui/renderGameBoards";
 
-const body = document.querySelector('body')
 
-const test = document.createElement('h1')
-test.textContent = ('hello')
-test.classList.add('test')
-
-body?.appendChild(test)
-
-// let me: Player = new Player('me', 'computer')
-// let computer: Player = new Player('computer', 'me')
-
-// const players = [me, computer]
-
-// me.attack(10, computer)
+renderGrids('jjcrazyman')
---
+++ src/ui/renderGameBoards.ts
@@ -0,0 +1,39 @@
+import { GameBoard } from "../factories/gameboard"
+import { Player } from "../factories/players"
+
+export default function renderGrids(playerName: string){
+
+const body = document.querySelector('body')
+
+const grid1 = document.querySelector('.grid-container1')
+const title1 = document.querySelector('.title-1')!
+title1.textContent = playerName
+
+const grid2 = document.querySelector('.grid-container2')
+const title2 = document.querySelector('.title-2')!
+title2.textContent = "Computer"
+
+let human = new Player("human", "computer")
+let computer = new Player("computer", "human")
+let board = human.gameboard.board
+
+for(let i = 0; i < board.length; i++){
+    for(let j = 0; j < board[i].length; j++){
+        const gridItem = document.createElement('div')
+        gridItem.setAttribute("position", board[i][j])
+        // gridItem.textContent = board[i][j]
+        gridItem.classList.add('grid-item')
+        grid1?.appendChild(gridItem)
+    }
+}
+
+for(let i = 0; i < board.length; i++){
+    for(let j = 0; j < board[i].length; j++){
+        const gridItem = document.createElement('div')
+        gridItem.setAttribute("position", board[i][j])
+        // gridItem.textContent = board[i][j]
+        gridItem.classList.add('grid-item')
+        grid2?.appendChild(gridItem)
+    }
+}
+}
